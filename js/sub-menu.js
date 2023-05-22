f9f19320d86e9a802001f8b3a7f980a905a94a80
Ieesus Chumpitaz <ichumpitaz17@gmail.com>
2023-05-22 10:27:36 -0500

    se modifico el html y el css de la pagina de bienvenida, se termino de darle los retoques a la pagina de figurasGEo y se corrigio los estilos css, ademas se m odifico el JS del aside  para hacerlo mas legible.

--- js/sub-menu.js
+++ js/sub-menu.js
@@ -1,20 +1,52 @@
-let listElement = document.querySelectorAll('.list__button--click');
+/* variables globales */
+const botonTaller1 = document.querySelector(".boton-encendido");
+const botonTaller3 = document.querySelector(".boton-stats");
+const navDespegable1 = document.querySelector(".nav__despegable");
+const navDespegable3 = document.querySelector(".nav__despegable-stats");
+const sectionCuadrado = document.getElementById("section-cuadrado");
+const sectionTriangulo = document.getElementById("section-triangulo");
+const sectionCirculo = document.getElementById("section-circulo");
+const linkCuadrado = document.querySelector(".link-cuadrado");
+const linkTriangulo = document.querySelector(".link-triangulo");
+const linkCirculo = document.querySelector(".link-circulo");
 
-listElement.forEach(listElement => {
-    listElement.addEventListener('click',()=>{
-        
-        listElement.classList.toggle('arrow');
+/* others */
 
-        let height = 0;
-        let menu = listElement.nextElementSibling;
-        console.log(menu.scrollHeight);
-        console.log(menu);
-        if(menu.clientHeight == "0"){
-            height=menu.scrollHeight;
-        }
-        menu.style.height= `${height}px`;
-        console.log(menu.style.height);
-    
-    })
-    
-});
+sectionTriangulo.style.display = "none";
+sectionCirculo.style.display = "none";
+
+/* escuchador de eventos */
+botonTaller1.addEventListener("click", openListElements);
+botonTaller3.addEventListener("click", openListElementsStats);
+linkCuadrado.addEventListener("click", openSectionCuadrado);
+linkTriangulo.addEventListener("click", openSectionTriangulo);
+linkCirculo.addEventListener("click", openSectionCirculo);
+
+/* funciones */
+function openListElements() {
+  navDespegable1.classList.toggle("active");
+}
+function openListElementsStats() {
+  navDespegable3.classList.toggle("active");
+}
+function openSectionCuadrado() {
+  /* escondemos las sections */
+  sectionTriangulo.style.display = "none";
+  sectionCirculo.style.display = "none";
+  /* abrimos la seccion cuadrado */
+  sectionCuadrado.style.display = "grid";
+}
+function openSectionTriangulo() {
+  /* escondemos las sections */
+  sectionCuadrado.style.display = "none";
+  sectionCirculo.style.display = "none";
+  /* abrimos la seccion triangulo */
+  sectionTriangulo.style.display = "grid";
+}
+function openSectionCirculo() {
+  /* escondemos las sections */
+  sectionTriangulo.style.display = "none";
+  sectionCuadrado.style.display = "none";
+  /* abrimos la seccion circulo */
+  sectionCirculo.style.display = "grid";
+}
